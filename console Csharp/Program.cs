3225fc59da548ca4c13402bd44e822fad1f316e8
wattsans <enzo.radici@viacesi.fr>
2023-03-06 15:43:13 +0100

    commit

--- console Csharp/Program.cs
+++ console Csharp/Program.cs
@@ -2,16 +2,37 @@
 
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        Console.Write("Saisir le salaire brut annuel : ");
-        double salaireBrutAnnuel = double.Parse(Console.ReadLine());
+        Console.Write("Veuillez saisir votre salaire annuel : ");
+        double salaireAnnuel = double.Parse(Console.ReadLine());
 
-        Console.Write("Saisir le taux d'imposition (en pourcentage) : ");
-        double tauxImposition = double.Parse(Console.ReadLine()) / 100.0;
+        double[] salairesMensuels = new double[12];
 
-        double salaireNetMensuel = (salaireBrutAnnuel / 12) * (1 - tauxImposition);
+        double salaireMensuelBase = salaireAnnuel / 12;
 
-        Console.WriteLine("Le salaire net mensuel est de {0:F2} euros.", salaireNetMensuel);
+        for (int i = 0; i < 11; i++)
+        {
+            salairesMensuels[i] = salaireMensuelBase;
+        }
+
+        double salaireDecembre = salaireMensuelBase + (salaireAnnuel * 0.1) / 12;
+        salairesMensuels[11] = salaireDecembre;
+
+        string[] moisDeLannee = { "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre" };
+
+        for (int i = 0; i < 12; i++)
+        {
+            if (i == 7)
+            {
+                Console.WriteLine(moisDeLannee[i] + " : entreprise fermée");
+            }
+            else
+            {
+                Console.WriteLine(moisDeLannee[i] + " : " + salairesMensuels[i] + "€");
+            }
+        }
+
+        Console.ReadLine();
     }
 }
